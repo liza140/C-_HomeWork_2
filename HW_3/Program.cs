8dd4a6bd34d34061254fe35950d8af691011bef1
liza140 <liza140@mail.ru>
2022-04-20 22:50:51 +0300

    Кратные числа

---
+++ HW_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 3. Семинар 2.
+
+int a = 34;
+int b = 5;
+if (a % b == 0) Console.WriteLine("Кратно");
+else
+{
+    Console.Write("Не кратно, остаток ");
+    Console.WriteLine(a % b);
+}
+
