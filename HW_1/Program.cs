ef688e658faa60c6a896c1d5d76dc566be91c023
liza140 <liza140@mail.ru>
2022-04-20 21:12:40 +0300

    Поиск максимальной цифры двузначного числа

---
+++ HW_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 1. Семинар 2.
+
+int MaxNum(int N)
+{
+    int a = N % 10;
+    int b = N / 10;
+    int max = a;
+    if (b > max) max = b;
+    return max;
+}
+int Num = new Random().Next(10, 100);
+Console.WriteLine(Num);
+Console.WriteLine(MaxNum(Num));
