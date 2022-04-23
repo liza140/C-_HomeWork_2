e28696de4cb085ee2f8c2fba16c327fa44161aef
liza140 <liza140@mail.ru>
2022-04-23 18:24:32 +0300

    Вторая цифра трехзначного числа

--- HW_2/Program.cs
+++ HW_2/Program.cs
@@ -1,22 +1,16 @@
 ﻿// Задача 2. Семинар 2.
 
-//Код в соответсвии с алгоритмом:
-
-//int N = 782;
-//if (N > 99 && N < 1000) Console.WriteLine((N / 10) % 10);
-//else if (N < -99 && N > -1000) Console.WriteLine(((-N) / 10) % 10);
-//else Console.WriteLine("Число не является трехзначным");
 
 
 //Код с методом:
 int SecNum(int N)
 {
-    int sec = -1;
-    if (N > 99 && N < 1000) sec = (N / 10) % 10;
-    if (N < -99 && N > -1000) sec = ((-N) / 10) % 10;
-    return sec;
+    if (N<0) N=-N;
+    if (N > 99 && N < 1000) return (N / 10) % 10;
+    else return -1;
+
 }
-int num = 782;
+int num = 82;
 if (SecNum(num) == -1) Console.WriteLine("Число не является трехзначным");
 else Console.WriteLine(SecNum(num));
 
